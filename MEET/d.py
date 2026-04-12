d9dc0b73c30347c0ba2d54d1ddbfaace63203882
Meet Mewada <164760112+Manmeet179@users.noreply.github.com>
2026-04-12 18:31:09 +0530

    Update d.py

--- MEET/d.py
+++ MEET/d.py
@@ -88,44 +88,6 @@ st.markdown(
     @keyframes colorchange {{
         0%   {{ color: #ff0000; }}
         2.5% {{ color: #ff3300; }}
-        5%   {{ color: #ff6600; }}
-        7.5% {{ color: #ff9900; }}
-        10%  {{ color: #ffcc00; }}
-        12.5%{{ color: #ffff00; }}
-        15%  {{ color: #ccff00; }}
-        17.5%{{ color: #99ff00; }}
-        20%  {{ color: #66ff00; }}
-        22.5%{{ color: #33ff00; }}
-        25%  {{ color: #00ff00; }}
-        27.5%{{ color: #00ff33; }}
-        30%  {{ color: #00ff66; }}
-        32.5%{{ color: #00ff99; }}
-        35%  {{ color: #00ffcc; }}
-        37.5%{{ color: #00ffff; }}
-        40%  {{ color: #00ccff; }}
-        42.5%{{ color: #0099ff; }}
-        45%  {{ color: #0066ff; }}
-        47.5%{{ color: #0033ff; }}
-        50%  {{ color: #0000ff; }}
-        52.5%{{ color: #3300ff; }}
-        55%  {{ color: #6600ff; }}
-        57.5%{{ color: #9900ff; }}
-        60%  {{ color: #cc00ff; }}
-        62.5%{{ color: #ff00ff; }}
-        65%  {{ color: #ff00cc; }}
-        67.5%{{ color: #ff0099; }}
-        70%  {{ color: #ff0066; }}
-        72.5%{{ color: #ff0033; }}
-        75%  {{ color: #ff1493; }}
-        77.5%{{ color: #ff4500; }}
-        80%  {{ color: #ff6347; }}
-        82.5%{{ color: #ffa500; }}
-        85%  {{ color: #ffd700; }}
-        87.5%{{ color: #adff2f; }}
-        90%  {{ color: #7fff00; }}
-        92.5%{{ color: #00fa9a; }}
-        95%  {{ color: #00ced1; }}
-        97.5%{{ color: #1e90ff; }}
         100% {{ color: #8a2be2; }}
     }}
     </style>
