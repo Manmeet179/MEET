31f66c2be6c876182438a6ef5a84d283db4ed889
Meet Mewada <164760112+Manmeet179@users.noreply.github.com>
2026-06-02 16:59:44 +0530

    Update d.py

--- MEET/d.py
+++ MEET/d.py
@@ -606,9 +606,7 @@ def account_records_page():
             .map(color_name, subset=["name"])
             .map(color_payment, subset=["payment_status"])
         )
-        
-        df["amount"] = df["amount"].apply(lambda x: int(x) if pd.notnull(x) else x)
-        
+
         st.dataframe(styled_df, use_container_width=True)
 
 
